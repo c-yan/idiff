e83ab0a1395b3a5a4d0b26139c399413cd936836
c-yan <c-yan@users.noreply.github.com>
2018-06-16 09:13:07 +0900

    Updated to v1.5.0 (2002/11/19)
    Changed to display maximum difference

--- main.pas
+++ main.pas
@@ -22,14 +22,9 @@ type
     { Public êÈåæ }
   end;
 
-  TRGBColor = record
-    B: Byte;
-    G: Byte;
-    R: Byte;
-  end;
-  PRGBColor = ^TRGbColor;
-  TRGBColorArray = Array[0..1600000] of TRGBColor;
-  PRGBColorArray = ^TRGBColorArray;
+  TByteTriple = packed array[0..2] of Byte;
+  TByteTripleArray = array[0..400000] of TByteTriple;
+  PByteTripleArray = ^TByteTripleArray;
 
 var
   MainForm: TMainForm;
@@ -38,9 +33,6 @@ implementation
 
 {$R *.DFM}
 
-var
-  LastResult: Extended = 0;
-
 procedure TMainForm.FormCreate(Sender: TObject);
 begin
   DragAcceptFiles(Handle, True);
@@ -48,16 +40,16 @@ end;
 
 procedure TMainForm.WMDropFiles(var Msg: TWMDropFiles);
 var
-  FileName: TFileName;
+  FileName: string;
 begin
   if DragQueryFile(Msg.Drop, $FFFFFFFF, nil, 0) = 2 then
   begin
-    SetLength(FileName, MAX_PATH + 1);
+    SetLength(FileName, MAX_PATH);
     DragQueryFile(Msg.Drop, 0, PChar(FileName), MAX_PATH);
     SetLength(FileName, StrLen(PChar(FileName)));
     FileName1Edit.Text := FileName;
 
-    SetLength(FileName, MAX_PATH + 1);
+    SetLength(FileName, MAX_PATH);
     DragQueryFile(Msg.Drop, 1, PChar(FileName), MAX_PATH);
     SetLength(FileName, StrLen(PChar(FileName)));
     FileName2Edit.Text := FileName;
@@ -69,47 +61,49 @@ end;
 procedure TMainForm.ExecButtonClick(Sender: TObject);
 var
   Bmp1, Bmp2: TBitmap;
-  X, Y: Integer;
-  Diff: Int64;
-  P1, P2: PRGBColorArray;
+  I, X, Y, Z, M, N: Integer;
+  P1, P2: PByteTripleArray;
 begin
   Bmp1 := TBitmap.Create;
   Bmp2 := TBitmap.Create;
 
-  try
-    Bmp1.LoadFromFile(FileName1Edit.Text);
-    Bmp2.LoadFromFile(FileName2Edit.Text);
+  Bmp1.LoadFromFile(FileName1Edit.Text);
+  Bmp2.LoadFromFile(FileName2Edit.Text);
 
-    Bmp1.PixelFormat := pf24bit;
-    Bmp2.PixelFormat := pf24bit;
+  if (Bmp1.Width <> Bmp2.Width) or (Bmp1.Height <> Bmp2.Height) then
+  begin
+    StatusBar.SimpleText := 'Size mismatch';
+    Exit;
+  end;
 
-    if (Bmp1.Width <> Bmp2.Width) or (Bmp1.Height <> Bmp2.Height) then
+  Bmp1.PixelFormat := pf24bit;
+  Bmp2.PixelFormat := pf24bit;
+
+  Z := 0;
+  M := 0;
+  for Y := 0 to Bmp1.Height - 1 do
+  begin
+    P1 := Bmp1.ScanLine[Y];
+    P2 := Bmp2.ScanLine[Y];
+    for X := 0 to Bmp1.Width - 1 do
     begin
-      StatusBar.SimpleText := 'Size mismatch';
-      Exit;
-    end;
-
-    Diff := 0;
-    for Y := 0 to Bmp1.Height - 1 do
-    begin
-      P1 := Bmp1.ScanLine[Y];
-      P2 := Bmp2.ScanLine[Y];
-      for X := 0 to Bmp1.Width - 1 do
+      for I := 0 to 2 do
       begin
-        Diff := Diff + Abs(P1[X].R - P2[X].R) + Abs(P1[X].G - P2[X].G) + Abs(P1[X].B - P2[X].B);
+        N := Abs(P1[X][I] - P2[X][I]);
+        Inc(Z, N);
+        if (N > M) then M := N;
       end;
     end;
-    LastResult := Diff / (Bmp1.Width * Bmp1.Height) / 3;
-    StatusBar.SimpleText := 'Result: ' + FloatToStr(LastResult);
-  finally
-    Bmp1.Free;
-    Bmp2.Free;
   end;
+  StatusBar.SimpleText := Format('Result: %.8f / %d', [Z / (Bmp1.Width * Bmp1.Height) / 3, M]);
+
+  Bmp1.Free;
+  Bmp2.Free;
 end;
 
 procedure TMainForm.CopyButtonClick(Sender: TObject);
 begin
-  Clipboard.AsText := FloatToStr(LastResult);
+  Clipboard.AsText := StatusBar.SimpleText;
 end;
 
 end.
